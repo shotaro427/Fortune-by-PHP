181a77e190512706e75cd38bf6d4eee468d1e379
toshinobu <hoge@email>
2019-07-12 11:57:03 +0900

    create fortune.php & index.php

---
+++ fortune.php
@@ -0,0 +1,29 @@
+
+<?php
+// -- おみくじの処理 -- 
+$fortunes = array('大吉', '中吉', '小吉', '末吉', '吉', '凶', '大凶');
+$fortune = $fortunes[rand(0, count($fortune-1))];
+
+// -- 日付の取得 --
+// - タイムゾーンの設定 -
+date_default_timezone_get('Asia/Tokyo');
+
+// - $dateにその日の日付を代入 -
+$date = date("Y年m月d日");
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>Document</title>
+</head>
+<body>
+    <p class="text-center"><?php echo($date); ?>の運勢は・・・</p>
+    <h1 class="text-center"><?php echo($fortune); ?>です！</h1>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,15 +1,15 @@
-
-
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.css">
     <title>Document</title>
 </head>
 <body>
-    <h1><?php echo 'Hello,World'; ?></h1>
+    <h1 class="text-center">おみくじへようこそ！</h1>
+    <a href="fortune.php" class="btn btn-primary">Let's start!!</a>
 </body>
 </html>
 
