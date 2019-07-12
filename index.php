7af7fc841091003901f4555718424d66ba8e6eb3
toshinobu <hoge@email>
2019-07-12 12:24:32 +0900

    create fortune.php & index.php style.css

--- index.php
+++ index.php
@@ -5,11 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
-    <h1 class="text-center">おみくじへようこそ！</h1>
-    <a href="fortune.php" class="btn btn-primary">Let's start!!</a>
+    <div class="container">
+        <h1 class="text-center">おみくじへようこそ！</h1>
+        <a href="fortune.php" class="btn btn-primary">Let's start!!</a>
+    </div>
 </body>
 </html>
 
